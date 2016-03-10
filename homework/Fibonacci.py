01bff5da0a4c0c387eb808d54160db12e56187b6
mikelei8291 <mikelei8291@gmail.com>
2016-03-10 21:47:01 +0800

    Fix some symbol problem in Fibonacci numbers

--- homework/Fibonacci.py
+++ homework/Fibonacci.py
@@ -10,10 +10,12 @@ n = int(input("How many Fibonacci numbers do you want? "))
 a = ((5**0.5)-1)/2
 b = ((5**0.5)+1)/2
 c = (5**0.5)/5
+print ("[", end="")
 for j in range(n):
     fibonacciValue = c*((b**j)-(a**j))
     #Python is stupid that it will calculate some
     #fibonacciValue to 1.000000000000002 so ceil
     #will convert it to 2, I just want to say "Fuck!"
     fibonacciNumber = int(ceil(fibonacciValue-0.1))
-    print (fibonacciNumber, end=" ")
+    print ("%i, " %fibonacciNumber, end="")
+print ("]")
