3cef73c28fbdab583dc6efeb080f7a63d16417b9
mikelei8291 <mikelei8291@gmail.com>
2016-03-11 11:27:41 +0800

    Learn selection

---
+++ school/selection.py
@@ -0,0 +1,20 @@
+name = str(input("Input something: "))
+age = int(input("Input an age: "))
+if name == "Luke":
+    print("Fuck it!")
+if name == "Android":
+    print("Best Mobile OS in the world!")
+if name == "iOS":
+    print("Laji!!! Such close, so limited, very stupid! wow!")
+else:
+    print("Say something!")
+if name == "Queen":
+    if age < 18:
+        print("Sorry!")
+    else:
+        print("Buy it!")
+else:
+    if age >= 18:
+        print("My boy, buy it!")
+    else:
+        print("Fuck off!")
