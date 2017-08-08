688dded8147baca497e790e7a618eeec8ae55d47
mikelei8291 <mikelei8291@gmail.com>
2017-08-08 17:35:53 +0800

    Exercise 6

---
+++ lpthw/ex6.py
@@ -0,0 +1,22 @@
+typesOfPeople = 10
+x = f"There are {typesOfPeople} types of people."
+
+binary = "binary"
+doNot = "don't"
+y = f"Those who know {binary} and those who {doNot}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+jokeEvaluation = "Isn't that joke so funny?! {}"
+
+print(jokeEvaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
