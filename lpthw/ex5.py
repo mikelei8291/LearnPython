620da25cab54122c200cc42c9fbd3438b6433036
mikelei8291 <mikelei8291@gmail.com>
2017-08-08 15:07:10 +0800

    Exercise 5

---
+++ lpthw/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Mike Lei'
+my_age = 17
+my_height = 185
+my_weight = 64.2
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimetres tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print("Actually that's too thin.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the food.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight}, I get {total}.")
