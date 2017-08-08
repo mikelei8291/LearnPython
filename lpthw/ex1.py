b993408977ebf2334844d975931ab7e8943908a1
mikelei8291 <mikelei8291@gmail.com>
2017-08-08 15:05:08 +0800

    Fix Syntax

--- lpthw/ex1.py
+++ lpthw/ex1.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
-print "Let me try 中文"
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("Let me try 中文")
--- lpthw/ex2.py
+++ lpthw/ex2.py
@@ -1,9 +1,9 @@
 # A comment
 # Anything after the # is ignored by Python
 
-print "I could have code like this." # and the comment after is ignored
+print("I could have code like this.")  # and the comment after is ignored
 
 # It can also disable a line of code
 # print "This won't run."
 
-print "This will run."
+print("This will run.")
