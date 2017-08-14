c82373f0693190f06eab8fd8116b6c627ea77918
mikelei8291 <mikelei8291@gmail.com>
2017-08-14 18:06:49 +0800

    Exercise 8,9,10

---
+++ lpthw/ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, True, False))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "I have",
+    "\nA lot of waifus",
+    "\nBut they all",
+    "\nLive in the animes"
+))
