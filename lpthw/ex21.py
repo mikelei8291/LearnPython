d34818d8f3ce96848c11bf061ab8feaa231ef6e0
mikelei8291 <mikelei8291@gmail.com>
2017-11-19 17:10:48 +0800

    Add exercises 19, 20, 21

---
+++ lpthw/ex21.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    print(f"Adding {a} + {b}")
+    return a + b
+
+
+def substract(a, b):
+    print(f"Substracting {a} - {b}")
+    return a - b
+
+
+def multiply(a, b):
+    print(f"Multiplying {a} * {b}")
+    return a * b
+
+
+def divide(a, b):
+    print(f"Dividing {a} / {b}")
+    return a / b
+
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = substract(78, 4)
+weight = multiply(90, 2)
+iq = divide(200, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, substract(height, multiply(weight, divide(iq, 2))))
+print("That becomes: ", what, "Can you do it by hand?")
